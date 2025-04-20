f100c215b8ffda850b64a49954b932d54685ba89
ubuntuhp <fortunato.didomenico@gmail.com>
2025-04-20 18:54:24 +0200

    click and input done

--- resources/views/pages/movie/play_movie.blade.php
+++ resources/views/pages/movie/play_movie.blade.php
@@ -1,13 +1,42 @@
 <x-layout>
     <h1 class="text-3xl">Movie to send</h1>
 
-        <p class="my-8">Movie found:</p>
+        {{-- <p class="my-8">Movie found:</p>
         @session('movies')
         <pre class="overflow-y-scroll bg-white rounded-sm shadow-xl p-4 w-[500px]" style="max-height: 500px">
             {{ session('movies') }} 
         </pre>
-        @endsession
+        @endsession --}}
+{{-- ----------------------------------------------------------------------------------------------- --}}
 
-        <p class="text-red-500">MANCA IL CLICK SUL FILM PER SELEZIONARLO E PLAY</p>
+
+
+<p class="my-8">Movie found:</p>
+<div class="my-4">
+    <input type="text" id="selectedMovie" class="shadow-sm border rounded-md p-2 w-full sm:w-96" placeholder="Seleziona un film">
+</div>
+<div class="overflow-y-scroll bg-white rounded-sm shadow-xl p-4 w-[500px]" style="max-height: 500px">
+    @session('movies')
+        @php
+            $moviesArray = explode("\n", session('movies'));
+        @endphp
+        <ul>
+            @foreach ($moviesArray as $movie)
+                <li class="cursor-pointer hover:bg-gray-100 p-2" onclick="selectMovie('{{ trim($movie) }}')">{{ trim($movie) }}</li>
+            @endforeach
+        </ul>
+    @endsession
+</div>
+
+<button class="btn bg-red-400 text-white mt-10">play</button>
+
+<script>
+    function selectMovie(movieName) {
+        document.getElementById('selectedMovie').value = movieName;
+    }
+</script>
+
+
+{{-- ------------------------------------------------------------------ --}}
         <button class="btn bg-red-400 text-white mt-10">play</button>
 </x-layout>
