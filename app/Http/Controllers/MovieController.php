7c99e66c3df97706f6a88c22238617919e7000fd
Fortunato Di Domenico <fortunato.didomenico@gmail.com>
2025-06-27 23:38:42 +0200

    throwable try and catch moviecontroller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,67 +2,73 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\MovieRequest;
+use Exception;
+use Spatie\Ssh\Ssh;
 use App\Models\Train;
 use Illuminate\Http\Request;
-use Spatie\Ssh\Ssh;
+use App\Http\Requests\MovieRequest;
+use Illuminate\Support\Facades\Log;
 
 class MovieController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
-        $cinema = range(1,26);
+        $cinema = range(1, 26);
 
         return view('pages.movie.index_movie', compact('cinema'));
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
 
         $train = $request->input('train');
         $search = $request->input('search');
 
-        exec('ping -c 3 -W 3 10.131.'.$train.'.1',$output,$ping); 
+        exec('ping -c 3 -W 3 10.131.' . $train . '.1', $output, $ping);
         // exec('wsl ping -c 3 -w 3 10.131.'.$train.'.1',$output,$ping); //todo WINDOWS
 
         if ($ping == 0) {
             if (!empty($search)) {
                 try {
                     //code...
-                    $movies = Ssh::create('developer','10.131.'.$train.'.1')->setTimeout(5)->execute('ls /media/movies | grep -i '.$search)->getOutput();
+                    $movies = Ssh::create('developer', '10.131.' . $train . '.1')->setTimeout(5)->execute('ls /media/movies | grep -i ' . $search)->getOutput();
                 } catch (\Exception $e) {
                     $movies = $e;
+                    Log::error('Errore nella creazione utente: ' . $e->getMessage());
+                    return redirect()->back()->with('output', $e);
                 }
-                
             } else {
                 try {
                     //code...
-                    $movies = Ssh::create('developer','10.131.'.$train.'.1')->setTimeout(5)->execute('ls /media/movies')->getOutput();
-                } catch (\Exception $e) {
+                    $movies = Ssh::create('developer', '10.131.' . $train . '.1')->setTimeout(5)->execute('ls /media/movies')->getOutput();
+                } catch (\Throwable $e) {
                     $movies = $e;
                 }
             }
             // return redirect()->route('movie.select',compact('train'))->with('movies',$movie);
-            return view('pages.movie.play_movie',compact('movies','train'));
-
+            return view('pages.movie.play_movie', compact('movies', 'train'));
         } else {
-            return redirect()->back()->with('unreachable','Box 1 del treno '.$train.' non raggiungilile!!');
+            return redirect()->back()->with('unreachable', 'Box 1 del treno ' . $train . ' non raggiungilile!!');
         }
-        
     }
 
 
-    public function play(Request $request){
+    public function play(Request $request)
+    {
 
         $train = $request->input('train');
         $film = $request->input('film');
-        $cmd="curl -s --show-error http://localhost:2323/cinema/server/play/";
+        $cmd = "curl -s --show-error http://localhost:2323/cinema/server/play/";
 
         try {
-            $output = Ssh::create('developer','10.131.'.$train.'.1 ')->execute($cmd.$film)->getOutput();
-        } catch (\Exception $e) {
-            $output=$e;
+            $output = Ssh::create('developer', '10.131.' . $train . '.1 ')->setTimeout(5)->execute($cmd . $film)->getOutput();
+        } catch (\Throwable $e) {
+            $output = $e;
+            Log::error('Errore nella creazione utente: ' . $e->getMessage());
+            return redirect()->back()->with('output', $e);
         }
-        
-        return redirect()->route('movie.index')->with('output',$output);
+
+        return redirect()->route('movie.index')->with('output', $output);
     }
 }
